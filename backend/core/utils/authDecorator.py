fd0700a3133cdcc6967061e8cd0944ca73b8de5c
Daniel Johnson <danielrjc@icloud.com>
2024-04-26 06:58:41 +0000

    Added requests library to decorator and fetching user

--- backend/core/utils/authDecorator.py
+++ backend/core/utils/authDecorator.py
@@ -1,12 +1,13 @@
 from functools import wraps
 import jwt
 from ...data.models.user import User
+from flask import request
 
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        jwtToken = args[0].headers.get('Authorization')
-        jwtToken = jwtToken.replace("Bearer ", "")
+        jwtToken = request.headers.get('Authorization', '').replace("Bearer ", "")
+        print(jwtToken)
         try:
             # ref: https://stackoverflow.com/questions/55703156/aws-cognito-how-to-decode-jwt-in-python
             decoded_token = jwt.decode(jwtToken, algorithms=["RS256"], options={"verify_signature": False})
